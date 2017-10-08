f220d2aa6d02c3992b5df4aa4f412d2ebcfd2012
maxcore25 <mkorobov2002@mail.ru>
2017-10-08 15:53:14 +0300

    Оптимальный вес

---
+++ 03/Оптимальный вес/Оптимальный вес/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Console.WriteLine("Здавствуйте! Пожалуйста, введите Ваш рост:");
+                double growth = double.Parse(Console.ReadLine());
+                Console.WriteLine("Пожалуйста, введите Ваш вес:");
+                double weight = double.Parse(Console.ReadLine());
+                int num = 100;
+                double optweight = growth - num;
+                Console.WriteLine("Ваш оптимальный вес составляет:" + optweight);
+                if (weight > optweight)
+                {
+                    Console.WriteLine("Вам стоит похудеть.");
+                }
+                else
+                {
+                    Console.WriteLine("Вам стоит поправиться");
+                }
+                Console.ReadLine();
+          
+        }
+    }
+}
