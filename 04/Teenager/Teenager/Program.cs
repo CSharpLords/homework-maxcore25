d9cc9ce38a6655d2b241bab91ac5a8193c8b4122
maxcore25 <mkorobov2002@mail.ru>
2017-10-15 01:14:13 +0300

    Teenager

---
+++ 04/Teenager/Teenager/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Пожалуйста, введите свой возраст:");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 10)
+            {
+                Console.WriteLine("Поздравляю! Ты - подросток. Устрой дестрой, порядок - это отстой!");
+            }
+            else
+            {
+                Console.WriteLine("К сожалению, ты мелкий...");
+                if (age > 20)
+                {
+                    Console.WriteLine("Эх, ты уже сишком стар. Ты - не подросток");
+                }
+                else
+                {
+                    Console.WriteLine("Поздравляю, ты - подросток. Устрой дестрой, порядок - это отстой!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
