cb7f0748cf8cdd4271d83a2591dfa11961809091
maxcore25 <mkorobov2002@mail.ru>
2017-10-15 00:28:23 +0300

    Space

---
+++ 02/Круг/Круг/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    class Circle
+    {
+        static void Main()
+        {
+            Console.WriteLine("Формула площади круга: PI * r * r");
+            Console.WriteLine("Введите длину радиуса:");
+            double radius = double.Parse(Console.ReadLine());
+            double area = 3 * radius * radius;
+            Console.WriteLine("S=" + area);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ 03/Сортировка/Сортировка/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя первого игрока:");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Введите рейтинг первого игрока:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите имя второго игрока:");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("Введите рейтинг второго игрока:");
+            double b = double.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("Результаты сортировки: " + name1 + " " + name2);
+            }
+            else
+            {
+                Console.WriteLine("Результаты сортировки: " + name2 + " " + name1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
