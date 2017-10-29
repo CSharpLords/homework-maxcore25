a8349cc2275bfc548d6c430193bcf9de16e3cd84
maxcore25 <mkorobov2002@mail.ru>
2017-10-29 12:36:34 +0300

    Correct SayHello

---
+++ SayHello2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       static void Main(string[] args)
+       {
+            Console.WriteLine("Здравствуйте! Пожалуйста, введите Ваше имя:");
+            string Name = Console.ReadLine();
+            SayHello(Name);
+       }
+        static void SayHello(string Name)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Привет," + Name + "!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ БМР.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Пожалуйста, введите первое число:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Пожалуйста, введите второе число:");
+            int b = int.Parse(Console.ReadLine());
+            Console.ReadLine(); 
+            if (a > b)
+            {
+                Console.WriteLine(">");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("<");
+            }
+            else if (a == b)
+            {
+                Console.WriteLine("=");
+            }
+            Console.ReadLine();
+        }
+    }
+}
