a27815946285b69114bcf5282945efb18e169959
maxcore25 <mkorobov2002@mail.ru>
2017-10-29 12:01:36 +0300

    SayHello

---
+++ SayHello.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello();
+            Console.ReadLine();
+        }
+        static void SayHello()
+        {
+            Console.WriteLine("Пожалуйста, введите Ваше имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name + "!");
+        }
+    }
+}
