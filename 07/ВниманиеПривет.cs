250cde404a7c3bba2a33d7af3c7516069bce6587
maxcore25 <mkorobov2002@mail.ru>
2017-11-19 13:28:40 +0300

    ВниманиеПривет

---
+++ 07/ВниманиеПривет.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowMessage("Привет!", false);
+            ShowMessage("Внимание! Внимание!", true);
+        }
+        static void ShowMessage(string message, bool isWarning)
+        {
+            Console.WriteLine(message);
+            if (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.ReadLine();
+            }
+        }
+    }
+}
