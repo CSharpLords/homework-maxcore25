77a478232f4e039f3d55cac4a98246ddcaae0b76
maxcore25 <mkorobov2002@mail.ru>
2017-12-03 03:12:49 +0300

    10.2.

---
+++ 10/10.2/10.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Пожалуйста, введите число:");
+            int number = int.Parse(Console.ReadLine());
+            int squaredNumber = number * number;
+            Console.WriteLine("Квадрат данного числа равен " + squaredNumber);
+            Console.ReadLine();
+        }
+    }
+}
