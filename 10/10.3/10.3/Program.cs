2d13fcc46d09b8ca7b292847d95cdef9d571beca
maxcore25 <mkorobov2002@mail.ru>
2017-12-03 04:02:16 +0300

    10.3.

---
+++ 10/10.3/10.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Пожалуйста, введите число:");
+            int number = int.Parse(Console.ReadLine());
+            int x = 0;
+            while (x < number)
+            { 
+               
+                x = x + 1;
+                int squaredX = x * x;
+                if (squaredX > number)
+                {
+                    break;
+                }
+                Console.WriteLine(squaredX);
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
