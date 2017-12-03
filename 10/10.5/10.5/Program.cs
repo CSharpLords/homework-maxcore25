43595aa0ed0fb1cf6cdc3195a9f69b657bf8464c
maxcore25 <mkorobov2002@mail.ru>
2017-12-03 04:43:05 +0300

    10.5.

---
+++ 10/10.5/10.5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Пожалуйста, введите число:");
+            double number = double.Parse(Console.ReadLine());
+            while ()
+            {
+                double powerOf3 = number / 3;
+            }
+            Console.ReadLine();
+        }
+    }
+}
