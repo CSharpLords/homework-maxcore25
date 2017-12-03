b28c8abc3a2564db01cee0aa08f242cf97719ac1
maxcore25 <mkorobov2002@mail.ru>
2017-12-03 04:03:53 +0300

    09.4.

---
+++ 09/09.4/09.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Пожалуйста, введите число:");
+                int number = int.Parse(Console.ReadLine());
+                sum = sum + number;
+                Console.WriteLine("Сумма введённых чисел: " + sum);
+            }
+
+        }
+    }
+}
