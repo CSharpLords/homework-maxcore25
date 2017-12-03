ada238e0f6bea7956ef6f0a1ef33b99b4e2a1b63
maxcore25 <mkorobov2002@mail.ru>
2017-12-03 04:25:25 +0300

    10.4.

---
+++ 10/10.4/10.4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравстсвуйте! Пожалуйста, введите грузоподъемность НЛО:");
+            int strengthUFO = int.Parse(Console.ReadLine());
+            Console.WriteLine("Пожалуйста, введите массу коров:");
+            int cowMass = int.Parse(Console.ReadLine());
+            if (strengthUFO > cowMass)
+            {
+                Console.WriteLine("Отсек для коров заполнен, взлетаем!");
+                double cowAmount = strengthUFO / cowMass;
+                Console.WriteLine("Количество коров, рзамещенных на борту:" + cowAmount);
+            }
+            else
+            {
+                Console.WriteLine("Слишком большой вес коров, отмена охоты!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
