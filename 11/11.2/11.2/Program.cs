e4b435676625c89cd9af9fa9f6bf6b8493406553
maxcore25 <mkorobov2002@mail.ru>
2017-12-10 01:19:15 +0300

    11.2.

---
+++ 11/11.2/11.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int sumOfN = 0;
+            double avarageSum = 0;
+            for ( int amountRand = 0; amountRand <= 50; amountRand = amountRand + 1)
+            {
+                
+                int n = rand.Next(50);
+                sumOfN = sumOfN + n;
+                Console.WriteLine(n);
+            }
+
+            avarageSum = sumOfN / 50;
+            Console.WriteLine("Среднее арифметическое: " + avarageSum);
+            Console.ReadLine();
+        }
+    }
+}
