c3f7677851e88f9e95b4282b244a682f8ca3bb59
maxcore25 <mkorobov2002@mail.ru>
2017-12-10 13:41:41 +0300

    Lines(12.3)

--- 12/Lines/Form1.cs
+++ 12/Lines/Form1.cs
@@ -20,14 +20,11 @@ namespace LoopyLandscape {
 			graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
 
 			Pen pen = new Pen(Brushes.BlueViolet, 8);
-			// Упростите код с помощью цикла for
-			graphics.DrawLine(pen, 20, 40, 80, 80); // первая пара чисел - координаты начала отрезка, вторая - координаты конца
-			graphics.DrawLine(pen, 80, 40, 140, 80);
-			graphics.DrawLine(pen, 140, 40, 200, 80);
-			graphics.DrawLine(pen, 200, 40, 260, 80);
-			graphics.DrawLine(pen, 260, 40, 320, 80);
-			graphics.DrawLine(pen, 320, 40, 380, 80);
-			graphics.DrawLine(pen, 380, 40, 440, 80);
+            for (int x = 20; x < 380; x = x + 60)
+            {
+                graphics.DrawLine(pen, x, 40, x + 60, 80); 
+            }
+			
 		}
 	}
 }
