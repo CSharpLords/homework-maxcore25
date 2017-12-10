51197793dbaf8d5f1bb4b349d852979820c8aa3e
maxcore25 <mkorobov2002@mail.ru>
2017-12-10 12:33:03 +0300

    11.5

---
+++ 11/11.5/11.5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Пожалуйста, введите слово: ");
+            string word = Console.ReadLine().ToLower();
+            int haveJe = 0;
+            for (int je = 0; je < word.Length; je++)
+            {
+                 if(word[je] == 'ж')
+                {
+                    haveJe++;
+                    Console.WriteLine("В слове " + word + " есть буква 'Ж'.");
+                    break;
+                }
+            }
+                if (haveJe == 0)
+                {
+                    Console.WriteLine("В слове " + word + " буква 'Ж' не найдена.");
+                }
+            Console.ReadLine();
+        }
+    }
+}
