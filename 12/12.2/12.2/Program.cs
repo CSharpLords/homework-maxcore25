c3e06d189b515dcda89be4408b27cdf201192dfa
maxcore25 <mkorobov2002@mail.ru>
2017-12-10 13:32:10 +0300

    12.2

---
+++ 12/12.2/12.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 0;
+            for (double number = 0.1; number < 0.9; number = number + 0.1)
+            {
+                result = Math.Sqrt(number);
+                Console.WriteLine("√" + number + " = " + result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
