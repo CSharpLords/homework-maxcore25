f27a0b7d2abbbb5c75883e3c2c8823403628ab8a
maxcore25 <mkorobov2002@mail.ru>
2018-01-28 00:17:25 +0300

    12.6

---
+++ 12/12.6/12.6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Пожалуйста, введите слово:");
+            string word = Console.ReadLine();
+            for (int letter = 0; letter < word.Length; letter++)
+            {
+                int end = word.Length - 1;
+                Console.WriteLine(word[end - letter]);
+            }
+                Console.ReadLine();
+        }
+    }
+}
