49a5399638f32f870514485990bc834235b7bcba
maxcore25 <mkorobov2002@mail.ru>
2018-02-11 14:28:05 +0300

    Stopwatch

---
+++ 18/Stopwatch/Assets/C#Scripts/Rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour 
+{
+	float time = 0;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		time = time + Time.deltaTime;
+		int seconds = (int)time;
+		transform.Rotate(new Vector3(0, 6, 0) * Time.deltaTime);
+
+		if (seconds >= 10) 
+		{
+			print (seconds);
+		}
+	}
+}
