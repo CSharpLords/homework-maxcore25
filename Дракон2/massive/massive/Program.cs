cb0d21d40a92204d3cd18d725690dd78c4dae9d6
maxcore25 <mkorobov2002@mail.ru>
2018-02-11 14:28:43 +0300

    какие-то массивы

---
+++ Дракон2/massive/massive/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace massive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1, 2, 3, 4, 5};
+            int sum = 0;
+
+            foreach (int el in numbers)
+            {
+                sum += el;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
