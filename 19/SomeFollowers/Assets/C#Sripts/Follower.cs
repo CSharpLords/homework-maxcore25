fd7bee5b4d2414850bf0fa9f3ab075ebfa3dcd7e
maxcore25 <mkorobov2002@mail.ru>
2018-02-18 17:02:33 +0300

    SomeFollowers

---
+++ 19/SomeFollowers/Assets/C#Sripts/Follower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour 
+{
+	 
+	public Transform[] objects;
+	float time = 0;
+	float min = 1000;
+	Transform nearestObj;
+	// Use this for initialization
+	void Start () 
+	{
+		for(int n = 0; n < objects.Length; n++)
+		{
+			float dist = Vector3.Distance(objects[n].position, transform.position);
+			if(dist < min)
+			{
+				min = dist;
+				nearestObj = objects [n];
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 direction = nearestObj.position - transform.position;
+		direction.Normalize ();
+		transform.Translate(direction * Time.deltaTime);
+	
+	}
+}
