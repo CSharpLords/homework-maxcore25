fd382167d770c3a755e04f44770f56022254aecb
maxcore25 <mkorobov2002@mail.ru>
2018-02-18 14:02:03 +0300

    Follower

---
+++ 19/Follower/Assets/C#Scripts/Follower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour 
+{
+	public Transform follower; 
+	public Transform target;
+	float time = 0; 
+	// Use this for initialization
+	void Start () 
+	{
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		time = 0 + Time.deltaTime;
+		transform.Translate(Vector3.left * Time.deltaTime);
+
+		Vector3 direction  = target.position - follower.position;
+		direction.Normalize();
+	}
+}
