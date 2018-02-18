04b349026789d57d668a6d5bf59d0fd919761fc4
maxcore25 <mkorobov2002@mail.ru>
2018-02-18 13:38:27 +0300

    ChangingColor

---
+++ 19/ChangingColour/Assets/C#Scripts/ChangeColor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour 
+{
+	Renderer render;
+	float time = 0;
+	// Use this for initialization
+	void Start () 
+	{
+		render = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		if (time > 5)
+		{
+			time = 0;
+			time = time + Time.deltaTime;
+			render.material.color = Color.Lerp(Color.white, Color.red, time / 5);
+		}
+		time = time + Time.deltaTime;
+		render.material.color = Color.Lerp (Color.red, Color.white, time / 5);
+	}
+}
