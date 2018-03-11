5733073217bf80bc19cf78267ec3d7032650f9f4
maxcore25 <mkorobov2002@mail.ru>
2018-03-11 12:42:34 +0300

    ColourOfCar

--- 22/ColourOfCar/Assets/C#Scripts/Red.cs
+++ 22/ColourOfCar/Assets/C#Scripts/Red.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotate : MonoBehaviour {
-
+public class Red : MonoBehaviour 
+{
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		transform.Rotate(new Vector3(0, 6, 0) * Time.deltaTime);
+	void Update () 
+	{
+		
+
 	}
+
 }
