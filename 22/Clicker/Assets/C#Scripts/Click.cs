29066dd1e9b49b1c3a9e146b0e9294b9cb5472f6
maxcore25 <mkorobov2002@mail.ru>
2018-03-18 13:11:39 +0300

    Clicker

---
+++ 22/Clicker/Assets/C#Scripts/Click.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Click : MonoBehaviour 
+{
+	
+	public Text scoreField;
+	public static int score = 0;
+	// Use this for initialization
+	void Start () 
+	{
+		Button plusPoint = GetComponent<Button> ();
+		plusPoint.onClick.AddListener (Increase);
+		scoreField.text = "0";
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	void Increase()
+	{
+		score = score + 1;
+		scoreField = score.ToString ();
+	}
+}
