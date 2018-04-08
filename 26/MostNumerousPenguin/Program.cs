40ec17a30f4e1dffd29bc41e679c5a3e211d076c
maxcore25 <mkorobov2002@mail.ru>
2018-04-08 16:13:33 +0300

    MostNumerousPenguin

--- 26/MostNumerousPenguin/Program.cs
+++ 26/MostNumerousPenguin/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MostNumerousPenguin {
-	class Program {
-		static void Main(string[] args) {
+namespace MostNumerousPenguin 
+{
+	class Program 
+    {
+		static void Main(string[] args) 
+        {
 			string mostNumerous1 = GetMostNumerous(new string[] { "Emperor Penguin", "Macaroni Penguin", "Emperor Penguin", "Little Penguin" });
 			string mostNumerous2 = GetMostNumerous(new string[] { "Emperor Penguin", "Macaroni Penguin", "Little Penguin", "Emperor Penguin", "Macaroni Penguin", "Macaroni Penguin", "Little Penguin" });
 			Console.WriteLine(mostNumerous1);
@@ -15,8 +18,39 @@ namespace MostNumerousPenguin {
 			Console.ReadLine();
 		}
 
-		static string GetMostNumerous(string[] penguins) {
-			return "";
+		static string GetMostNumerous(string[] penguins) 
+        {
+            int amountOfEP = 0;
+            int amountOfMP = 0;
+            int amountOfLP = 0;
+            for (int i = 0; i < penguins.Length; i++ )
+            {
+                if (penguins[i] == "Emperor Penguin")
+                {
+                    amountOfEP++;
+                }
+                if (penguins[i] == "Macaroni Penguin")
+                {
+                    amountOfMP++;
+                }
+                if (penguins[i] == "Little Penguin")
+                {
+                    amountOfLP++;
+                }
+            }
+            if(amountOfEP > amountOfLP && amountOfEP > amountOfMP)
+            {
+			    return "Emperor Penguin";
+            }
+            if (amountOfMP > amountOfLP && amountOfMP > amountOfEP)
+            {
+                return "Macaroni Penguin";
+            }
+            if (amountOfLP > amountOfMP && amountOfLP > amountOfEP)
+            {
+                return "Little Penguin";
+            }
+            return "";
 		}
 	}
 }
