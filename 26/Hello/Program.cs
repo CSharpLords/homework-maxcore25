70b5397afa389e5f51210c795122716b1c42f77c
maxcore25 <mkorobov2002@mail.ru>
2018-04-08 15:30:23 +0300

    Hello

--- 26/Hello/Program.cs
+++ 26/Hello/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hello {
-	class Program {
-		static void Main(string[] args) {
+namespace Hello 
+{
+	class Program 
+    {
+		static void Main(string[] args) 
+        {
 			SayHello("Маша");
 			SayHello("Женя");
 			SayHello("Петя");
@@ -14,8 +17,26 @@ namespace Hello {
 			Console.ReadLine();
 		}
 
-		static void SayHello(string name) {
-			// пиши код здесь
+		static void SayHello(string name) 
+        {
+            
+            Random rand = new Random();
+            int number = rand.Next(0, 3);
+            if (number == 0)
+            {
+                Console.WriteLine("Привет, " + name);
+            }
+            
+            else if(number == 1)
+            {
+                Console.WriteLine("Добрый день, " + name);
+            }
+            
+            else if(number == 2)
+            {
+                Console.WriteLine("Здравствуй, " + name);
+            }
+           
 		}
 	}
 }
