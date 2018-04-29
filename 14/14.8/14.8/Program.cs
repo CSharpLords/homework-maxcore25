aed92f3347ebf0ba4c64c3762da3cbf137e32317
maxcore25 <mkorobov2002@mail.ru>
2018-04-30 02:06:22 +0300

    14.8

---
+++ 14/14.8/14.8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = {12, 12, 45, 63, 2, 6, 63, 65, 97, 194};
+            int sumOfInt = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                sumOfInt += integers[i];
+            }
+            Console.WriteLine("The summ of the first six elements is " + sumOfInt);
+            Console.ReadLine();
+        }
+    }
+}
