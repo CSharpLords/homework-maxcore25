20f4b25e134c635f5eee5fa2494c4c24f985422a
maxcore25 <mkorobov2002@mail.ru>
2018-04-30 01:48:34 +0300

    CopyPasteHell

--- 14/CopyPasteHell/Program.cs
+++ 14/CopyPasteHell/Program.cs
@@ -4,21 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CopyPasteHell {
-	class Program {
-		static void Main(string[] args) {
-			float[] numbers = { 765, 341, 1, -3, 0, 13, -200, 23, 44 };
+namespace CopyPasteHell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] numbers = { 765, 341, 1, -3, 0, 13, -200, 23, 44 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Элемент номер " + i + " принимает значение " + numbers[i]);
+                /*numbers[0] = numbers[0] / 5;
+                numbers[1] = numbers[1] / 5;
+                numbers[2] = numbers[2] / 5;
+                numbers[3] = numbers[3] / 5;
+                numbers[4] = numbers[4] / 5;
+                numbers[5] = numbers[5] / 5;
+                numbers[6] = numbers[6] / 5;
+                numbers[7] = numbers[7] / 5;
+                numbers[8] = numbers[8] / 5;*/
+            }
 
-			numbers[0] = numbers[0] / 5;
-			numbers[1] = numbers[1] / 5;
-			numbers[2] = numbers[2] / 5;
-			numbers[3] = numbers[3] / 5;
-			numbers[4] = numbers[4] / 5;
-			numbers[5] = numbers[5] / 5;
-			numbers[6] = numbers[6] / 5;
-			numbers[7] = numbers[7] / 5;
-			numbers[8] = numbers[8] / 5;
-			Console.ReadLine();
-		}
-	}
+            Console.ReadLine();
+        }
+    }
 }
