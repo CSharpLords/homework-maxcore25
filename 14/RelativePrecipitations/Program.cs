5e5e38ae61ddf16121dc5a8a0fbfb80f3e1fe1d7
maxcore25 <mkorobov2002@mail.ru>
2018-04-30 02:20:11 +0300

    RelativePrecipitations

--- 14/RelativePrecipitations/Program.cs
+++ 14/RelativePrecipitations/Program.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RelativePrecipitations {
-	class Program {
-		static void Main(string[] args) {
+namespace RelativePrecipitations 
+{
+	class Program 
+    {
+		static void Main(string[] args) 
+        {
 			int[] precipitations = { 23, 4545, 45, 2, -16, 765, 341, 1, -3, 0 };
-
-			for (int i = 0; i < precipitations.Length; i++) {
+            Console.WriteLine("Количество осадков за первый месяц: " + precipitations[0]);
+			for (int i = 1; i < precipitations.Length; i++) 
+            {
 				precipitations[i] = precipitations[i] - precipitations[0];
-				Console.WriteLine(precipitations[i]);
+                Console.WriteLine("Изменение количества осадков относительно первого месяца" + precipitations[i]);
 			}
 			Console.ReadLine();
 		}
