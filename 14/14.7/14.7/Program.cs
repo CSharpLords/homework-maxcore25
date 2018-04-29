f6b0b5eb81bf1939e98325bd21b288a66f435d54
maxcore25 <mkorobov2002@mail.ru>
2018-04-30 01:59:01 +0300

    14.7

---
+++ 14/14.7/14.7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = {84, 54, 13, 45, 22, 68, 54};
+            int sumOfInt = 0;
+            for (int i = 0; i < integers.Length; i++)
+            {
+                sumOfInt += integers[i];
+            }
+            sumOfInt %= 2;
+            if(sumOfInt == 0)
+            {
+                Console.WriteLine("Cумма элементов массива является четным числом");
+            }
+            else
+            {
+                Console.WriteLine("Cумма элементов массива не является четным числом");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
