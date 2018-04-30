2790481840b9386d873552e209e774560fd1bce0
maxcore25 <mkorobov2002@mail.ru>
2018-04-30 21:43:14 +0300

    15.6

---
+++ 15/15.6/15.6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] growths = new int[12];
+            for (int i = 0; i < growths.Length; i++)
+            {
+                Random rand = new Random();
+                int exactGrowth = rand.Next(163, 191);
+                growths[i] = exactGrowth;
+                Console.WriteLine(growths[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
