9763f706d1125c41b542d4419b0057017c06ee26
maxcore25 <mkorobov2002@mail.ru>
2018-04-30 22:53:45 +0300

    15.7

---
+++ 15/15.7/15.7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] multiplesOfThree = new int[17];
+            int firstNumber = 60;
+            for (int i = 0; i < multiplesOfThree.Length; i++)
+            {
+                int residue = firstNumber % 3;
+                if(residue == 0)
+                {
+                    multiplesOfThree[i] = firstNumber;
+                    Console.WriteLine(multiplesOfThree[i]);
+                    firstNumber -= 3;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
