7561c59bc377c1300481f25e2ff4f31b241ba2d0
maxcore25 <mkorobov2002@mail.ru>
2018-04-30 18:39:03 +0300

    15.3

---
+++ 15/15.3/15.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = {1, 2, 34, 2, 45, 86, 93, 1, 0, 4};
+            for (int i = 1; i < integers.Length; i++)
+            {
+                if(integers[i] > integers[i - 1])
+                {
+                    Console.WriteLine(integers[i]); 
+                }
+                
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
