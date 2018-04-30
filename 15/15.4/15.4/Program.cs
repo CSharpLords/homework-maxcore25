6390079c7b6c97553ae33250d2d6b83d67e79d66
maxcore25 <mkorobov2002@mail.ru>
2018-04-30 21:01:36 +0300

    15.4

---
+++ 15/15.4/15.4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = { -43, 10, 9, -23, -54 };
+            int signPlus = 0;
+            int signMinus = 0;
+            for (int i = 0; i < integers.Length; i++)
+            {
+                if (integers[i] > 0)
+                {
+                    signPlus++;
+                    signMinus = 0;
+
+                }
+                if (integers[i] < 0)
+                {
+                    signMinus++;
+                    signPlus = 0;
+                }
+
+                if (signPlus == 2)
+                {
+                    Console.WriteLine(integers[i - 1] + " " + integers[i]);
+                    break;
+                }
+                if (signMinus == 2)
+                {
+                    Console.WriteLine(integers[i - 1] + " " + integers[i]);
+                    break;
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
