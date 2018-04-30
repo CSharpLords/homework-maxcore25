cb68bf33155061e7dfb133f6fedaed22537e84a4
maxcore25 <mkorobov2002@mail.ru>
2018-04-30 21:07:10 +0300

    PlaceFrogs

--- 15/PlaceFrogs/Form1.cs
+++ 15/PlaceFrogs/Form1.cs
@@ -11,17 +11,18 @@ namespace PlaceFrogs {
             InitializeComponent();
         }
 
-        protected override void OnPaint(PaintEventArgs e) {
+        protected override void OnPaint(PaintEventArgs e)
+        {
             graphics = e.Graphics;
             graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
 
             // рисуем кувшинки и лягушек
-            for (int i = 0; i < 1000; i += 250) {
+            for (int i = 0; i < 1000; i += 250)
+            {
                 DrawLilypad(i, 0);
+                DrawFrog(i + 50, 35);
             }
-            for (int i = 50; i < 1000; i += 250) {
-                DrawFrog(i, 35);
-            }
+
         }
 
         private void DrawFrog(int x, int y) {
