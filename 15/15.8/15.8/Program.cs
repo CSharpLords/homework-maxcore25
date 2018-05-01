d16e39d3ac53354442df6535ca989e71315f9ab6
maxcore25 <mkorobov2002@mail.ru>
2018-05-01 21:39:23 +0300

    15.8

---
+++ 15/15.8/15.8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumFirstShop = 0;
+            int sumSecondShop = 0;
+            int[] pricesOfFirstShop = new int[61];
+            Random rand = new Random();
+            for (int i = 0; i < pricesOfFirstShop.Length; i++)
+            {
+                int price = rand.Next(10, 10000);
+                pricesOfFirstShop[i] = price;
+                sumFirstShop += price;
+            }
+            int[] pricesOfSecondShop = new int[61];
+            for (int i = 0; i < pricesOfSecondShop.Length; i++)
+            {
+                int price = rand.Next(10, 10000);
+                pricesOfSecondShop[i] = price;
+                sumSecondShop += price;
+            }
+            Console.WriteLine("Общая стоимость проданных фирмой товаров за два месяца равна " + (sumFirstShop + sumSecondShop) + " рублей");
+            Console.ReadLine();
+        }
+    }
+}
