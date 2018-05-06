929155fa6027adfcd85fd22e2bac7297cb0052b5
maxcore25 <mkorobov2002@mail.ru>
2018-05-06 17:28:55 +0300

    SpaceRace

--- 30/SpaceRace/SpaceRace/Program.cs
+++ 30/SpaceRace/SpaceRace/Program.cs
@@ -3,30 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace SpaceRace
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            SpacePlane spacePlane = new SpacePlane();
-            spacePlane.FinishLine();
-            spacePlane.Flight();
-            /*SpacePlane[] allSpacePlanes = new SpacePlane[10];
+
+            Random rand = new Random();
+            
+            
+            
+            SpacePlane[] allSpacePlanes = new SpacePlane[10];
             for (int i = 1; i < allSpacePlanes.Length; i++)
             {
-                allSpacePlanes[i] = new SpacePlane();
+                SpacePlane spacePlane = new SpacePlane();
+                spacePlane.speed = rand.Next(3, 7);
+
+                allSpacePlanes[i] = spacePlane;
+            }
+            bool finishGame = false;
+            while (finishGame == false)
+            {
+                Console.Clear();
+                for (int i = 1; i < allSpacePlanes.Length; i++)
+                {
+                    allSpacePlanes[i].Flight();
+                    if (allSpacePlanes[i].x >= 50)
+                    {
+                        finishGame = true;
+                        
+                        
+                    }
+   
+                }
                 
-                Console.WriteLine("-");
+                FinishLine();
+
+                Thread.Sleep(1000);
                 
                 
-            }*/
-            
-            
+            }
+
+            Console.WriteLine("Final!!!");
             
             
             Console.ReadLine();
+        }
+
+        static void FinishLine()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.SetCursorPosition(50, i);
+                Console.WriteLine("|");
+            }
         }
     }
 }
--- 30/SpaceRace/SpaceRace/SpacePlane.cs
+++ 30/SpaceRace/SpaceRace/SpacePlane.cs
@@ -10,57 +10,26 @@ namespace SpaceRace
     class SpacePlane
     {
         public int speed;
-        public int distance = 6;
+
         public int x = 0;
         public int y = 10;
 
 
-        public void FinishLine()
-        {
 
-            for (int i = 0; i < 10; i++)
-            {
-                y -= 1;
-                Console.SetCursorPosition(50, y);
-                Console.WriteLine("|");
-
-            }
-            Console.SetCursorPosition(x, y);
-
-        }
-
-
-
-        Random rand = new Random();
         public void Flight()
         {
 
-
-            for (int i = 0; i < distance; i++)
+            string visualSpeed = "-";
+            x += speed;
+            for (int n = 1; n < x; n++)
             {
-                speed = rand.Next(3, 7);
-                string visualSpeed = "-";
-                for (int n = 1; n < speed; n++)
-                {
-                    visualSpeed = visualSpeed + "-";
-                    Console.Write(visualSpeed);
-                }
 
+                visualSpeed = visualSpeed + "-";
 
-                Thread.Sleep(1000);
-
-
-                if (i >= distance)
-                {
-                    WriteFinal();
-                    break;
-                }
             }
+            Console.WriteLine(visualSpeed);
 
         }
-        public void WriteFinal()
-        {
-            Console.WriteLine("Final!!!");
-        }
+
     }
 }
