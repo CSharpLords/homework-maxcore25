8bc0f515492512e0772788b04481a8b8f3369a5d
maxcore25 <mkorobov2002@mail.ru>
2018-05-06 14:00:11 +0300

    CustomRectangle

---
+++ 30/CustomRectangle/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRectangle
+{
+    class Rectangle
+    {
+        public int x;
+        public int y;
+        public int height;
+        public int width;
+        public string symbol;
+        public void Draw()
+        {
+            for (int i = 0; i < height; i++)
+            {
+                y += 1;
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine(symbol);
+
+
+            }
+            
+            
+            for (int j = 1; j < width; j++)
+            {
+
+                x += 1;
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine(symbol);
+            }
+            
+            
+        }
+    }
+}
