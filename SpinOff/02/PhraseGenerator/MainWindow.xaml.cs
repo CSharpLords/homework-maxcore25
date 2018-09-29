4b60a4b3e2503d9b681831c134463a722bd0df4d
maxcore25 <mkorobov2002@mail.ru>
2018-09-29 17:21:15 +0300

    PhraseGenerator

--- SpinOff/02/PhraseGenerator/MainWindow.xaml.cs
+++ SpinOff/02/PhraseGenerator/MainWindow.xaml.cs
@@ -20,33 +20,18 @@ namespace PhraseGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
-        Random random = new Random();
-        int indexNoun;
-        int indexAdverb;
-        int indexVerb;
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        private void NounsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            indexNoun = random.Next(0, NounsList.Items.Count);
-        }
-
-        private void AdverbsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            indexAdverb = random.Next(0, AdverbsList.Items.Count);
-        }
-
-        private void VerbsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            indexVerb = random.Next(0,AdverbsList.Items.Count);
-        }
-
+        
         private void GenerateButton_Click(object sender, RoutedEventArgs e)
         {
-            Text.Content = ((ListBoxItem)NounsList.Items[random.Next(0, NounsList.Items.Count)]).Content.ToString() + " " + ((ListBoxItem)AdverbsList.Items[random.Next(0, AdverbsList.Items.Count)]).Content.ToString() + " " + ((ListBoxItem)VerbsList.Items[random.Next(0, AdverbsList.Items.Count)]).Content.ToString();
+            Random random = new Random();
+            int indexNoun = random.Next(0, NounsList.Items.Count);
+            int indexAdverb = random.Next(0, AdverbsList.Items.Count);
+            int indexVerb = random.Next(0, AdverbsList.Items.Count);
+            Text.Content = ((ListBoxItem)NounsList.Items[indexNoun]).Content.ToString() + " " + ((ListBoxItem)AdverbsList.Items[indexAdverb]).Content.ToString() + " " + ((ListBoxItem)VerbsList.Items[indexVerb]).Content.ToString();
         }
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
