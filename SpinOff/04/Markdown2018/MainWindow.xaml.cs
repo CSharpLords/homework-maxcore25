1cfafab2a730faf003b31593f8f6197ab66c79ea
maxcore25 <mkorobov2002@mail.ru>
2018-10-29 16:13:25 +0300

    Markdown2018

--- SpinOff/04/Markdown2018/MainWindow.xaml.cs
+++ SpinOff/04/Markdown2018/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace Markdown2018
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Element> elements;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,23 +28,82 @@ namespace Markdown2018
 
         private void MarkdownTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
-        }
-
-        private void PreviewRichTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            StackPanel stackPanel = new StackPanel();
-            FlowDocument flowDocument = new FlowDocument();
-            Run run = new Run("hello");
-            Bold bold = new Bold(new Run(" world"));
             Paragraph paragraph = new Paragraph();
-            paragraph.Inlines.Add(run);
-            paragraph.Inlines.Add(bold);
+            elements = new List<Element>();
+
+            BoldType();
+            ItalicType();
+            UsualType();
+            elements = elements.OrderBy(el => el.position).ToList();
+            foreach (var element in elements)
+            {
+                paragraph.Inlines.Add(element.element);
+            }
+
+            FlowDocument flowDocument = new FlowDocument();
             flowDocument.Blocks.Add(paragraph);
-            RichTextBox richTextBox = new RichTextBox();
-            richTextBox.Document = flowDocument;
-            stackPanel.Children.Add(richTextBox);
-            this.Content = stackPanel;
+
+            PreviewRichTextBox.Document = flowDocument;
         }
+
+        private void BoldType()
+        {
+            string[] parts = MarkdownTextBox.Text.Split(new[] { "**" }, StringSplitOptions.None);
+            int start = 0;
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Bold bold = new Bold(new Run(parts[i]));
+                    Element boldType = new Element();
+                    boldType.element = bold;
+                    boldType.position = MarkdownTextBox.Text.IndexOf("**", start);
+                    start = boldType.position + 2 + parts[i].Length + 2;
+                    elements.Add(boldType);
+                }
+            }
+        }
+
+        private void ItalicType()
+        {
+            string[] parts = MarkdownTextBox.Text.Split(new[] { "*" }, StringSplitOptions.None);
+            int start = 0;
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Italic italic = new Italic(new Run(parts[i]));
+                    Element italicType = new Element();
+                    italicType.element = italic;
+                    italicType.position = MarkdownTextBox.Text.IndexOf("*", start);
+                    start = italicType.position + 2 + parts[i].Length;
+                    elements.Add(italicType);
+                }
+            }
+        }
+
+        private void UsualType()
+        {
+            string[] parts = MarkdownTextBox.Text.Split(new[] { "*"}, StringSplitOptions.None);
+            int start = 0;
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Run run = new Run(parts[i]);
+                    Element usualType = new Element();
+                    usualType.element = run;
+                    usualType.position = MarkdownTextBox.Text.IndexOf("*", start);
+                    start = usualType.position + parts[i].Length;
+                    elements.Add(usualType);
+                }
+            }
+        }
+    }
+
+    class Element
+    {
+        public Inline element;
+        public int position;
     }
 }
