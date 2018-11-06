b34179cbba26ca3990d7ec8e89604661b2d8091f
maxcore25 <mkorobov2002@mail.ru>
2018-11-07 00:33:14 +0300

    Markdown2

--- SpinOff/04/Markdown2/MainWindow.xaml.cs
+++ SpinOff/04/Markdown2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Security.AccessControl;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,41 +22,69 @@ namespace Markdown2
         private void MarkdownTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             int amountOfStars = 0;
+            bool italicIsActivated = false;
             StringBuilder italicString = new StringBuilder();
+            bool boldIsActivated = false;
+            StringBuilder boldString = new StringBuilder();
 
             FlowDocument flowDocument = new FlowDocument();
             Paragraph paragraph = new Paragraph();
             flowDocument.Blocks.Add(paragraph);
             PreviewRichTextBox.Document = flowDocument;
 
-            string text = "a*s*кот*s*u";
-            //string text = MarkdownTextBox.Text;
+            //string text = "a**кот**u";
+            string text = MarkdownTextBox.Text;
 
             for (int i = 0; i < text.Length; i++)
             {
-                if (amountOfStars == 1 && text[i] == '*')
+                if (text[i] == '*' && text[i + 1] == '*') //must correct this condition
                 {
-                    Italic italic = new Italic(new Run(italicString.ToString()));
-                    paragraph.Inlines.Add(italic);
-                    italicString.Clear();
-                    amountOfStars = 0;
+                    boldIsActivated = true;
+                    amountOfStars += 2;
+                    if (i >= text.Length)
+                    {
+                        i--;
+                    }
+                    if (boldIsActivated == true && amountOfStars == 4) 
+                    {
+                        Bold bold = new Bold(new Run(boldString.ToString()));
+                        paragraph.Inlines.Add(bold);
+                        boldString.Clear();
+                        amountOfStars = 0;
+                        boldIsActivated = false;
+                    }
                 }
                 else
                 {
-                    if (text[i] == '*')
+                    if (amountOfStars == 1 && text[i] == '*')
                     {
-                        amountOfStars++;
+                        Italic italic = new Italic(new Run(italicString.ToString()));
+                        paragraph.Inlines.Add(italic);
+                        italicString.Clear();
+                        amountOfStars = 0;
+                        italicIsActivated = false;
                     }
                     else
                     {
-                        if (amountOfStars == 1)
+                        if (text[i] == '*')
                         {
-                            italicString.Append(text[i]);
+                            italicIsActivated = true;
                         }
-                        else
+                        else // letters 
                         {
-                            Run run = new Run(text[i].ToString());
-                            paragraph.Inlines.Add(run);
+                            if (italicIsActivated == true)
+                            {
+                                italicString.Append(text[i]);
+                            }
+                            if (boldIsActivated == true)
+                            {
+                                boldString.Append(text[i]);
+                            }
+                            else
+                            {
+                                Run run = new Run(text[i].ToString());
+                                paragraph.Inlines.Add(run);
+                            }
                         }
                     }
                 }
