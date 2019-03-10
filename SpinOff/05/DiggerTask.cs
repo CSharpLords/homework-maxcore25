3c81c3b70f71b8faddd1a3a88823f1e88ac4d945
maxcore25 <mkorobov2002@mail.ru>
2019-03-10 22:57:31 +0300

    Digger

---
+++ SpinOff/05/DiggerTask.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digger
+{
+    class Player : ICreature
+    {
+        public string GetImageFileName()
+        {
+            return "Digger.png";
+        }
+
+        public int GetDrawingPriority()
+        {
+            return 1;
+        }
+
+        public CreatureCommand Act(int x, int y)
+        {
+            return new CreatureCommand();
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            return false;
+        }
+    }
+
+    class Terrain : ICreature
+    {
+        public string GetImageFileName()
+        {
+            return "Terrain.png";
+        }
+
+        public int GetDrawingPriority()
+        {
+            return 1;
+        }
+
+        public CreatureCommand Act(int x, int y)
+        {
+            return new CreatureCommand();
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            if (conflictedObject is Player)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+   
+}
