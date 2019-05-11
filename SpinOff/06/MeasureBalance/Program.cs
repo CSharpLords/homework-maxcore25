b8d0813893e1a97b3c78ae4add52409245f48324
Alex <iseldomused@gmail.com>
2019-05-12 01:30:07 +0300

    add 06/MeasureBalance

---
+++ SpinOff/06/MeasureBalance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MeasureBalance
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var balance = new Balance();
+			balance.AddRight(10);
+			balance.AddLeft(5);
+			balance.AddLeft(5);
+			Console.WriteLine(balance.Weigh()); // InBalance
+
+			balance.AddLeft(1);
+			Console.WriteLine(balance.Weigh()); // Left
+
+			balance.AddRight(2);
+			Console.WriteLine(balance.Weigh()); // Right
+
+			Console.WriteLine(balance.Weigh() is Enum); // true
+
+			Console.ReadLine();
+		}
+	}
+}
