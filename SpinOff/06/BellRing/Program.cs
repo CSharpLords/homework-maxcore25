3ace3672c8b9d4346f39a8b92f93efd3ff359adf
Alex <iseldomused@gmail.com>
2019-05-12 01:10:24 +0300

    add 06/BellRing

---
+++ SpinOff/06/BellRing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BellRing {
+	class Program {
+		static void Main(string[] args) {
+			var bell = new Bell();
+			bell.Sound(); // ding
+			bell.Sound(); // dong
+			bell.Sound(); // ding
+			Console.ReadLine();
+		}
+	}
+}
