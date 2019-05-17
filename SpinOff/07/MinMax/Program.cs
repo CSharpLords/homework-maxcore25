824d03a4433efa20d3b7aaede7b159d3bed0623c
Alex <iseldomused@gmail.com>
2019-05-18 02:07:58 +0300

    add SpinOff\07

---
+++ SpinOff/07/MinMax/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinMax {
+	class Program {
+		static void Main(string[] args) {
+			var finder = new MinMaxWordFinder();
+			finder.AddSentence("hello abc world");
+			finder.AddSentence("def asdf qwert");
+			Console.WriteLine(string.Join(", ", finder.GetShortestWords())); // abc, def
+			Console.WriteLine(string.Join(", ", finder.GetLongesttWords())); // hello, qwert, world
+
+			finder.AddSentence("  abc     table    ");
+			finder.AddSentence("world");
+			finder.AddSentence("def");
+			Console.WriteLine(string.Join(", ", finder.GetShortestWords())); // abc, def
+			Console.WriteLine(string.Join(", ", finder.GetLongesttWords())); // hello, qwert, table, world
+
+			finder.AddSentence("pool");
+			finder.AddSentence("people");
+			Console.WriteLine(string.Join(", ", finder.GetShortestWords())); // abc, def
+			Console.WriteLine(string.Join(", ", finder.GetLongesttWords())); // people
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ SpinOff/07/Paragraphs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Paragraphs {
+	class Program {
+		static void Main(string[] args) {
+			var lp = new LeftParagraph(8);
+			lp.AddWord("abc");
+			lp.AddWord("defg");
+			lp.AddWord("hi");
+			lp.AddWord("jklmnopq");
+			lp.AddWord("r");
+			lp.AddWord("stuv");
+			/* выведется
+			abc defg
+			hi
+			jklmnopq
+			r stuv
+			 */
+			Console.WriteLine(lp.Get());
+
+			var rp = new RightParagraph(8);
+			rp.AddWord("abc");
+			rp.AddWord("defg");
+			rp.AddWord("hi");
+			rp.AddWord("jklmnopq");
+			rp.AddWord("r");
+			rp.AddWord("stuv");
+			/* выведется
+			abc defg
+				  hi
+			jklmnopq
+			  r stuv
+			 */
+			Console.WriteLine(rp.Get());
+
+			Console.ReadLine();
+		}
+	}
+}
